da777b5261482f182169def3693b5959e96845bc
mohamed esmail <dev.mohamed.esmail@gmail.com>
2025-05-27 14:10:16 +0400

    fix the problem of image

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,6 +1,17 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "img-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
+        },
+      },
+    },
+  },
   'strapi::security',
   'strapi::cors',
   'strapi::poweredBy',
